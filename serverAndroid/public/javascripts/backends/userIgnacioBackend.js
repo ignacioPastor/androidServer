10d9c3d8dca0b2aa207d20343860282f7df53fad
ignacioPastor <ignacioPastor@users.noreply.github.com>
2017-10-14 13:15:39 +0100

    code cleaning

--- serverAndroid/public/javascripts/backends/userIgnacioBackend.js
+++ serverAndroid/public/javascripts/backends/userIgnacioBackend.js
@@ -5,31 +5,23 @@ var Promise = require('promise');
 
 
 exports.getUserByEmail = function (email){
-    console.log("userIgnacioBackendGetByEmail---------------1");
     let myUser = new Promise((fulfill, reject) => {
-    console.log("userIgnacioBackendGetByEmail---------------2");
         models.UserIgnacio.findOne({where: {email: email}, raw: true}).then(user => {
             if(user) {
-    console.log("userIgnacioBackendGetByEmail---------------3");
                 fulfill(user);
             }
             else {
-    console.log("userIgnacioBackendGetByEmail---------------4");
                 fulfill(null);
             }
         }).catch(reject);
     });
-    console.log("userIgnacioBackendGetByEmail---------------5");
     return myUser;
 };
 
 exports.updateUser = function (user) {
-    console.log("userIgnacioBackendUpdateUser---------------1");
     return new Promise((fulfill, reject) => {
-    console.log("userIgnacioBackendUpdateUser---------------2");
         models.UserIgnacio.update(user,{where: {id: user.id}})
             .then(rows => {
-    console.log("userIgnacioBackendUpdateUser---------------3");
                 fulfill({success: true});
             })
             .catch(reject);
@@ -37,13 +29,9 @@ exports.updateUser = function (user) {
 };
 
 exports.addUser = function (user) {
-    console.log("userIgnacioBackendAddUser---------------1");
     return new Promise((fulfill, reject) => {
-    console.log("userIgnacioBackendAddUser---------------2");
         models.UserIgnacio.create(user)
             .then(result => {
-                
-    console.log("userIgnacioBackendAddUser---------------3");
                 fulfill(result)
             })
             .catch(reject);
@@ -52,11 +40,8 @@ exports.addUser = function (user) {
 
 //Function to get all the clients from the database
 exports.getAll = function() {
-    console.log("userIgnacioBackendGetAll----------------1");
     return new Promise((fulfill, reject) => {
-    console.log("userIgnacioBackendGetAll----------------2");
         models.UserIgnacio.findAll({order: 'telefono asc', raw: true}).then(users => {
-    console.log("userIgnacioBackendGetAll----------------3");
             return fulfill(users);
         }).catch(reject);
     })
